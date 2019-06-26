dd215a8a47845206f5a38eaf4bb108cfc48ac6bd
zhongjt2016 <594464333@qq.com>
2019-06-26 16:37:48 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    //个人详情页
     public function show(User $user)
     {
-        return view('usres.show', compact('user'));
+        return view('users.show', compact('user'));
+    }
+
+    //编辑页
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //更新个人资料
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
