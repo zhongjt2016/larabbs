3017a53cd44c826f55b22592755ab39b76bea350
zhongjt2016 <594464333@qq.com>
2019-07-03 14:03:57 +0000

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,13 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         //读取分类id关联的话题，并按每20条分页
-        $topics = Topic::where('category_id', $category->id)->paginate(30);
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->paginate(30);
+        
         //传参变量话题和分类到模板中
         return view('topics.index', compact('topics', 'category'));
     }
